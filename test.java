6dd99fecece8d21aa009ea4cd0ff1f34686daaa0
Yasir Abdullah <yasirmabdullah1@hotmail.com>
2021-11-08 16:17:59 +0000

    added library for stemming

--- test.java
+++ test.java
@@ -1,22 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import opennlp.tools.stemmer.PorterStemmer;
 
 public class test {
     public static void main(String[] args) throws FileNotFoundException {
         
-        File myfile = new File("./src/main/resources/ham");
-        File[] myfiles = myfile.listFiles();
-        int i = 0;
-        for(File f: myfiles){
-            if(!f.isDirectory()){
-                i++;
-                Scanner s = new Scanner(f);
-                System.out.println(s.nextLine());
-                if(i>5){
-                    break;
-                }
-            }
-        } 
+        PorterStemmer s = new PorterStemmer();
+        System.out.println(s.stem("jumping"));
     }
 }
--- testcsv.java
+++ testcsv.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
